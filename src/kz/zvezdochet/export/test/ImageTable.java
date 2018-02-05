e056f2ecfb9f8518c71eca1aa93a214f39ce4636
Nataly Didenko <inbox@zvezdochet.guru>
2018-02-05 18:14:47 +0600

    Тестирование инфографики для конфигураций аспектов

--- src/kz/zvezdochet/export/test/ImageTable.java
+++ src/kz/zvezdochet/export/test/ImageTable.java
@@ -9,7 +9,6 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -28,52 +27,58 @@ public class ImageTable {
         PdfPTable table = new PdfPTable(3);
         //first row
 		PdfPCell cell = new PdfPCell();
-		cell.setBorder(Rectangle.TOP);
 		table.addCell(cell);
 
 		Paragraph p = new Paragraph("vertex");
 		p.setAlignment(Element.ALIGN_CENTER);
 		cell = new PdfPCell();
-		cell.setBorder(Rectangle.TOP);
 		cell.addElement(p);
 		table.addCell(cell);
 
 		cell = new PdfPCell(new Phrase());
-		cell.setBorder(Rectangle.TOP);
 		table.addCell(cell);
 
 		//second row
 		cell = new PdfPCell();
-		cell.setBorder(Rectangle.NO_BORDER);
 		table.addCell(cell);
 
-		String imgfile = "/home/nataly/workspace/kz.zvezdochet.analytics/icons/conf/palm.gif";
+		String imgfile = "/home/nataly/workspace/kz.zvezdochet.analytics/icons/conf/sail.gif";
 		com.itextpdf.text.Image image = com.itextpdf.text.Image.getInstance(imgfile);
 		cell = new PdfPCell(image);
-		cell.setBorder(Rectangle.NO_BORDER);
 		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cell.setRowspan(2);
 		table.addCell(cell);
 
 		cell = new PdfPCell(new Phrase());
-		cell.setBorder(Rectangle.NO_BORDER);
 		table.addCell(cell);
 
 		//third row
 		p = new Paragraph("left");
 		p.setAlignment(Element.ALIGN_RIGHT);
 		cell = new PdfPCell();
-		cell.setBorder(Rectangle.BOTTOM);
+		cell.setVerticalAlignment(Element.ALIGN_BOTTOM);
 		cell.addElement(p);
-		table.addCell(cell);
-
-		cell = new PdfPCell();
-		cell.setBorder(Rectangle.BOTTOM);
 		table.addCell(cell);
 
 		p = new Paragraph("right");
 		cell = new PdfPCell();
-		cell.setBorder(Rectangle.BOTTOM);
+		cell.setVerticalAlignment(Element.ALIGN_BOTTOM);
 		cell.addElement(p);
+		table.addCell(cell);
+
+        //fourth row
+		cell = new PdfPCell();
+		table.addCell(cell);
+
+		p = new Paragraph("base");
+		p.setAlignment(Element.ALIGN_CENTER);
+		cell = new PdfPCell();
+//		cell.setUseAscender(true);
+		cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+		cell.addElement(p);
+		table.addCell(cell);
+
+		cell = new PdfPCell(new Phrase());
 		table.addCell(cell);
 
 		document.add(table);
